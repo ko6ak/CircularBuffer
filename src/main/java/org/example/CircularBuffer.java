bbbbb284ee82bf105a557b7bc9c230927587704f
Den <ko6ak@ya.ru>
2024-02-23 20:15:21 +0300

    add openAPI specs

--- src/main/java/org/example/CircularBuffer.java
+++ src/main/java/org/example/CircularBuffer.java
@@ -1,11 +1,19 @@
 package org.example;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+@Tag(name = "CircularBuffer", description = "Циклический буфер для хранения любых объектов")
 public class CircularBuffer<T> {
     public static final String EMPTY_BUFFER = "Buffer is empty!";
 
+    @Schema(description = "Размер буфера", example = "100")
     private final int n;
     private final List<T> buffer;
     private int writeIndex;
@@ -18,12 +26,20 @@ public class CircularBuffer<T> {
         buffer = new CopyOnWriteArrayList<>();
     }
 
+    @Operation(summary = "Добавляет элемент в буфер")
     public synchronized void put(T item) {
         if (buffer.size() == n) buffer.set(writeIndex, item);
         else buffer.add(item);
         writeIndex = (writeIndex + 1) % n;
     }
 
+    @Operation(summary = "Забирает элемент из буфера",
+            responses = {
+                    @ApiResponse(description = "Возвращает элемент", content = @Content(mediaType = "Object")),
+                    @ApiResponse(description = "Возвращает ошибку \"Buffer is empty!\" если буфер пуст",
+                            content = @Content(mediaType = "EmptyBufferException"))
+            }
+    )
     public synchronized T get() {
         if (buffer.isEmpty()) throw new EmptyBufferException(EMPTY_BUFFER);
         if (buffer.size() <= readIndex) return null;
@@ -32,6 +48,7 @@ public class CircularBuffer<T> {
         return item;
     }
 
+    @Operation(summary = "Выводит содержимое буфера в консоль")
     public String print(){
         return buffer.toString();
     }
